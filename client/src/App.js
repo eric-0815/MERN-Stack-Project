8bbd84411740fc42751e6d8c50f65504f976066a
eric-0815 <67636157+eric-0815@users.noreply.github.com>
2021-10-26 13:05:27 +1100

    update

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,10 @@ import Login from "./components/auth/Login";
 import Alert from "./components/layout/Alert";
 import PrivateRoute from "./components/routing/PrivateRoute";
 import Dashboard from "./components/dashboard/Dashboard";
-
+import CreateProfile from "./components/proflie-form/CreateProfile";
+import EditProfile from "./components/proflie-form/EditProfile";
+import AddExperience from "./components/proflie-form/AddExperience";
+import AddEducation from "./components/proflie-form/AddEducation";
 // Redux
 import { Provider } from "react-redux";
 import store from "./store";
@@ -34,6 +37,26 @@ const App = () => {
               <Route exact path="/register" component={Register} />
               <Route exact path="/login" component={Login} />
               <PrivateRoute exact path="/dashboard" component={Dashboard} />
+              <PrivateRoute
+                exact
+                path="/create-profile"
+                component={CreateProfile}
+              />
+              <PrivateRoute
+                exact
+                path="/edit-profile"
+                component={EditProfile}
+              />
+              <PrivateRoute
+                exact
+                path="/add-experience"
+                component={AddExperience}
+              />
+              <PrivateRoute
+                exact
+                path="/add-education"
+                component={AddEducation}
+              />
             </Switch>
           </section>
         </Fragment>
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -142,7 +142,7 @@ router.get("/user/:user_id", async (req, res) => {
 router.delete("/", auth, async (req, res) => {
   try {
     // @todo - remove users posts
-    // Remove profile
+    await Post.deleteMany({ user: req.user.id }); // Remove profile
     await Profile.findOneAndRemove({ user: req.user.id });
     // Remove user
     await User.findByIdAndRemove({ _id: req.user.id });
